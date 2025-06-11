52b1faa31d61d201dd588161691eb97c02fef179
Sparsh Bansal <sparshbansal787@gmail.com>
2025-06-11 15:16:48 +0530

    server's api created

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+
+app.listen(PORT,() => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
